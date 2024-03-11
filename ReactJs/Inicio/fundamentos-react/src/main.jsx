2ccb3aab09cd394362fa99aefa94b1c02d3b98ef
Ana_D <ana.marcacini@maua.br>
2024-03-11 10:45:21 -0300

    aula 1 react

---
+++ ReactJs/Inicio/fundamentos-react/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react' /// core do react
+import ReactDOM from 'react-dom/client'// interação do react com a dom Document Object Model || ambiente web
+import App from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render( //renderiza a div root
+//Componetes react:
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
---
+++ ReactJs/Projeto 1/README.md
@@ -0,0 +1,14 @@
+# Rocketseat
+
+
+![Alt text](Notas/Screenshot_20240307_133918.png)
+
+'''
+anamarcacini in Rocketseat on  main [?] took 20,8s 
+➜  npm --version
+10.2.4
+anamarcacini in Rocketseat on  main [?] 
+➜  node --version
+v20.11.1
+
+'''
