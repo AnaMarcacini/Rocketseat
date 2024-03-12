ed880391333c0e406f2a6ee2ac3d9516824211a0
Ana_D <ana.marcacini@maua.br>
2024-03-12 15:37:22 -0300

    sidebar e post

--- ReactJs/Inicio/fundamentos-react/src/App.jsx
+++ ReactJs/Inicio/fundamentos-react/src/App.jsx
@@ -1,8 +1,14 @@
 import PostPrincipal from './Post' // Default Exports 
+import {Post} from "./components/Post" 
 import { Post2 } from './Post2' // Named Exports 
 import { Header } from './components/Header';
 // todos os componetnes possuem extenção jsx = JavaScript + XML // tem html interno
+
+import styles from './App.module.css';
+
 import './global.css';
+import { Sidebar } from './components/Sidebar';
+
 
 export function App() {
 
@@ -14,11 +20,23 @@ export function App() {
     <div>
       <Header/>
       <h1>Olá Mundo</h1>
-      <PostPrincipal/>
-      <Post2 autor = "Ana" conteudo ="Escrever 'lorem ' no vcode gera o texto automatico Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam dicta temporibus deserunt soluta magni iusto. Nobis nam odit culpa, nihil voluptatum quaerat reprehenderit sit ratione reiciendis maxime vero, esse neque. "/>
-      <Post2 
-        autor = "Ana2" 
-        conteudo ="Escrever 'lorem ' no vcode gera o texto automatico Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam dicta temporibus deserunt soluta magni iusto. Nobis nam odit culpa, nihil voluptatum quaerat reprehenderit sit ratione reiciendis maxime vero, esse neque. "/>
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post/>
+          <Post/>
+          <Post/>
+          <Post/>
+          <Post/>
+          <PostPrincipal/>
+          <Post2 autor = "Ana" conteudo ="Escrever 'lorem ' no vcode gera o texto automatico Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam dicta temporibus deserunt soluta magni iusto. Nobis nam odit culpa, nihil voluptatum quaerat reprehenderit sit ratione reiciendis maxime vero, esse neque. "/>
+          <Post2 
+            autor = "Ana2" 
+            conteudo ="Escrever 'lorem ' no vcode gera o texto automatico Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam dicta temporibus deserunt soluta magni iusto. Nobis nam odit culpa, nihil voluptatum quaerat reprehenderit sit ratione reiciendis maxime vero, esse neque. "/>
+        </main>
+
+      </div>
+
     </div>
   )
 }
--- ReactJs/Inicio/fundamentos-react/src/components/Header.jsx
+++ ReactJs/Inicio/fundamentos-react/src/components/Header.jsx
@@ -1,12 +1,15 @@
 import styles from './Header.module.css'; // importação de um module.css 
 // diferente do css normal o ele precisa da importação
 //  por convenção criamos componentes com a letra maiuscula para não confundir com os elementos do html
+import igniteLogo from '../assets/ignite-logo.svg';
 
 console.log(styles)
+console.log(igniteLogo)
 export function Header() {
   return (
     <header className={styles.header}>
       <strong>Ignite Feed</strong>
+      <img src={igniteLogo} alt="Logo" />
     </header>
   );
 }
