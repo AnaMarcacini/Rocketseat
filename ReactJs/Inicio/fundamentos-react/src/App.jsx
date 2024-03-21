a9a7577c2becd417caf10e317ece6e5bf6eb7044
Ana_D <ana.marcacini@maua.br>
2024-03-21 13:30:46 -0300

    inicio da programação

--- ReactJs/Inicio/fundamentos-react/src/App.jsx
+++ ReactJs/Inicio/fundamentos-react/src/App.jsx
@@ -8,7 +8,40 @@ import styles from './App.module.css';
 
 import './global.css';
 import { Sidebar } from './components/Sidebar';
+// json
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2042-03-03 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator @Rocketseat'
+    },
+    content: [ // vetor de objetos
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2024-03-03 20:00:00'),
+  },
+];
 
+
+// Nunca colocar html no json -> perigo de alguem malicioso colocar typescript e fazer rodar algo malicioso no seu navegador
 
 export function App() {
 
@@ -23,16 +56,20 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post/>
-          <Post/>
-          <Post/>
-          <Post/>
-          <Post/>
-          <PostPrincipal/>
-          <Post2 autor = "Ana" conteudo ="Escrever 'lorem ' no vcode gera o texto automatico Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam dicta temporibus deserunt soluta magni iusto. Nobis nam odit culpa, nihil voluptatum quaerat reprehenderit sit ratione reiciendis maxime vero, esse neque. "/>
-          <Post2 
-            autor = "Ana2" 
-            conteudo ="Escrever 'lorem ' no vcode gera o texto automatico Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam dicta temporibus deserunt soluta magni iusto. Nobis nam odit culpa, nihil voluptatum quaerat reprehenderit sit ratione reiciendis maxime vero, esse neque. "/>
+          {/* {posts.forEach(post =>{ // for each percorre o array porem não tem nenhum retorno por isso não funciona
+            <Post/>
+          } )} */}
+          {posts.map(post => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
+
         </main>
 
       </div>
