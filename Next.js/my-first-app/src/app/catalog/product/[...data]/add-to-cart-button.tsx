cf018fc65c49b11b3241c9b3524fb46210ae797e
Ana_D <ana.marcacini@maua.br>
2024-04-08 12:47:49 -0300

    abstração do use client

---
+++ Next.js/my-first-app/src/app/catalog/product/[...data]/add-to-cart-button.tsx
@@ -0,0 +1,19 @@
+'use client' // continua rodando no também no servidor porem manda o js para o cliente
+// componente await não é recomendado par client components
+// html da pagina de um componente de servidor é gerado uma unica vez -> não sofre mudanças no futuro (componente é gerado uma unica vez) diferente do component -> por isso é ruim de usar um use client como uma função async 
+// um client component não é garantido que será executado uma unica vez então irá buscar os dados e esperar no await varias vezes
+
+// usar useEffect para buscar dados de api e não o await
+
+import { useState } from 'react'
+
+export function AddToCartButton() {
+  const [count, setCount] = useState(0)
+
+  function addToCart() {
+    setCount((state) => state + 1)
+    console.log(count)
+  }
+
+  return <button onClick={addToCart}>Adicionar ao carrinho {count}</button>
+}
--- Next.js/my-first-app/src/app/catalog/product/[...data]/page.tsx
+++ Next.js/my-first-app/src/app/catalog/product/[...data]/page.tsx
@@ -1,9 +1,6 @@
-'use client' // continua rodando no também no servidor porem manda o js para o cliente
-// componente await não é recomendado par client components
-// html da pagina de um componente de servidor é gerado uma unica vez -> não sofre mudanças no futuro (componente é gerado uma unica vez) diferente do component -> por isso é ruim de usar um use client como uma função async 
-// um client component não é garantido que será executado uma unica vez então irá buscar os dados e esperar no await varias vezes
 
-// usar useEffect para buscar dados de api e não o await
+import { AddToCartButton } from './add-to-cart-button'
+
 interface ProductProps {
   params: {
     data: string[]
@@ -11,23 +8,20 @@ interface ProductProps {
 }
 
 // Server Components => A gente não USA JavaScript no lado do cliente
-// Client Components => O JavaScript é enviado ao navegador (cliente) TODO O JS SERÁ ENVIADO PARA O NAVEGADOR --> porem boa parte do codigo a baixo é estático assim podemos separar os dados que precisam ser use clients em um componente a parte para evitar o envio de js para o navegador faremos isso a seguir no prox commit 
-
-export default function Product({ params }: ProductProps) {
+// Client Components => O JavaScript é enviado ao navegador (cliente) TODO O JS SERÁ ENVIADO PARA O NAVEGADOR --> porem boa parte do codigo a baixo é estático assim podemos separar os dados que precisam ser use clients em um componente a parte para evitar o envio de js para o navegador
+export default async function Product({ params }: ProductProps) {
 
   const [productId, size, color] = params.data
   console.log(params) // printa isso no terminal se o 'use client' estiver comentado ou seja no servidor e se o 'use client' estiver descomentado isso printa no inspencionar do chrome e também no servidor
-
-  function addToCart(){
-    console.log("adicionou no carrinho")
-  }
+  const response = await fetch('https://api.github.com/users/diego3g')
+  const user = await response.json()
   //server components = padrão do next => não usa javascript no lado do cliente
   return (
     <div>
       <p>Product: {productId}</p>
       <p>Size: {size}</p>
       <p>Color: {color}</p>
-      <button onClick={addToCart}>AdicionarCarrinho</button>
+      <AddToCartButton />
     </div>
 
   )
