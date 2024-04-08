42e01637ad2c57c6f0ebe87efb2a8e014a5f42e4
Ana_D <ana.marcacini@maua.br>
2024-04-08 12:10:40 -0300

    client component- antes da abstração do catalog product

--- Next.js/my-first-app/src/app/catalog/product/[...data]/page.tsx
+++ Next.js/my-first-app/src/app/catalog/product/[...data]/page.tsx
@@ -1,12 +1,20 @@
-'use client' // 
+'use client' // continua rodando no também no servidor porem manda o js para o cliente
+// componente await não é recomendado par client components
+// html da pagina de um componente de servidor é gerado uma unica vez -> não sofre mudanças no futuro (componente é gerado uma unica vez) diferente do component -> por isso é ruim de usar um use client como uma função async 
+// um client component não é garantido que será executado uma unica vez então irá buscar os dados e esperar no await varias vezes
 
+// usar useEffect para buscar dados de api e não o await
 interface ProductProps {
   params: {
     data: string[]
   }
 }
 
+// Server Components => A gente não USA JavaScript no lado do cliente
+// Client Components => O JavaScript é enviado ao navegador (cliente) TODO O JS SERÁ ENVIADO PARA O NAVEGADOR --> porem boa parte do codigo a baixo é estático assim podemos separar os dados que precisam ser use clients em um componente a parte para evitar o envio de js para o navegador faremos isso a seguir no prox commit 
+
 export default function Product({ params }: ProductProps) {
+
   const [productId, size, color] = params.data
   console.log(params) // printa isso no terminal se o 'use client' estiver comentado ou seja no servidor e se o 'use client' estiver descomentado isso printa no inspencionar do chrome e também no servidor
 
---
+++ Next.js/my-first-app/src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function CatalogLoading() {
+    return <p>Carregando catálogo...</p>
+    // arquivo loading ele roda enquanto o arquivo page.tsx não carrega
+    // ele herda o cabeçalho
+
+    // esse arquivo serve para todas as paginas filhas do caminho /app a não ser que eu sobrescreva
+  }
