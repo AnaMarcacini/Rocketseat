8a99797013280252e8ec5728fe900696d1d8fc5f
Ana_D <ana.marcacini@maua.br>
2024-04-18 13:41:20 -0300

    componentes renderizados de forma assincrona

--- Next.js/my-first-app/src/app/page.tsx
+++ Next.js/my-first-app/src/app/page.tsx
@@ -1,13 +1,20 @@
 import { GithubProfile } from "@/components/github-profile"
+import { Suspense } from "react" // suspender 
+
+/* a pagina inteira espera o GithubProfile carregar para carregar os demais conteudos (exemplo o h1) */
+/* o next por padrão aguarda toda a página carregar para renderizar para o usuario */
+// o suspense eu consigo carregar de forma assincrona (componentes lentos não atrasam componentes rápidos)
 import { LongWaitComponent } from "@/components/long-wait-component"
 export default function Home() {
 
 
   return <div>
     <h1>Home!</h1>
-    <GithubProfile/> 
-    <LongWaitComponent/>
-    {/* a pagina inteira espera o GithubProfile carregar para carregar os demais conteudos (exemplo o h1) */}
-    {/* o next por padrão aguarda toda a página carregar para renderizar para o usuario */}
+    <Suspense fallback={<p>Isso é mostrando enquanto o GithubProfile não carrega</p>}>
+      <GithubProfile />
+    </Suspense>
+    <Suspense fallback={<p>Isso é mostrando enquanto o LongWaitComponent não carrega</p>}>
+      <LongWaitComponent />
+    </Suspense>
   </div>
 }
