c38f4380e0f443355a47d4fae89135eda57be7e8
Ana_D <ana.marcacini@maua.br>
2024-04-18 13:36:45 -0300

    componentes sendo renderizados juntos

--- Next.js/my-first-app/src/app/page.tsx
+++ Next.js/my-first-app/src/app/page.tsx
@@ -1,13 +1,13 @@
-export default async function Home() {
+import { GithubProfile } from "@/components/github-profile"
+import { LongWaitComponent } from "@/components/long-wait-component"
+export default function Home() {
 
-  await new Promise((resolve) => setTimeout(resolve, 2000))// 2 segundos// função assincrona async || o await faz o node (nextserver) aguarda todas as instruções que tem o await desse componente finalizarem para renderizar (return) é possivel ver o tempo de demora em network
 
-  const response = await fetch('https://api.github.com/users/diego3g') // espera o retorno da api
-  const user = await response.json() // tranforma o resultado da requisição em json e espera ele finalizar essa operação
-// Streaming SSR => Ler/escrever dados de forma parcial + Server-side Rendering --> Renderizar um componente pelo lado do servidor de forma PARCIAL --> na mesma requisição ele sobe os dados e o restante dos dados da api (atualiza os dados )
-
-// basicamente uma mesma requisição pode trazer dados parciais de uma pagina e ao longo do tempo pode ir adicionando mais informações na tela
-
-  // await é usado para dados que devem ser carregados imediatamente
-  return <pre>{JSON.stringify(user, null, 2)}</pre> //faz a formatação certinha  null, 2
+  return <div>
+    <h1>Home!</h1>
+    <GithubProfile/> 
+    <LongWaitComponent/>
+    {/* a pagina inteira espera o GithubProfile carregar para carregar os demais conteudos (exemplo o h1) */}
+    {/* o next por padrão aguarda toda a página carregar para renderizar para o usuario */}
+  </div>
 }
--- Next.js/my-first-app/src/components/github-profile.tsx
+++ Next.js/my-first-app/src/components/github-profile.tsx
@@ -1,8 +1,14 @@
-export async function GithubProfile() {
-    await new Promise((resolve) => setTimeout(resolve, 2000))
+export async function GithubProfile() { // sempre que tiver um elemento await devemos usar uma função assincrona async
+    await new Promise((resolve) => setTimeout(resolve, 2000))// 2 segundos// função assincrona async || o await faz o node (nextserver) aguarda todas as instruções que tem o await desse componente finalizarem para renderizar (return) é possivel ver o tempo de demora em network
   
     const response = await fetch('https://api.github.com/users/diego3g')
-    const user = await response.json()
+    // espera o retorno da api
+    const user = await response.json() // tranforma o resultado da requisição em json e espera ele finalizar essa operação
+    // Streaming SSR => Ler/escrever dados de forma parcial + Server-side Rendering --> Renderizar um componente pelo lado do servidor de forma PARCIAL --> na mesma requisição ele sobe os dados e o restante dos dados da api (atualiza os dados )
+    
+    // basicamente uma mesma requisição pode trazer dados parciais de uma pagina e ao longo do tempo pode ir adicionando mais informações na tela
+    
+      // await é usado para dados que devem ser carregados imediatamente
   
-    return <pre>{JSON.stringify(user, null, 2)}</pre>
+    return <pre>{JSON.stringify(user, null, 2)}</pre>//faz a formatação certinha  null, 2
   }
