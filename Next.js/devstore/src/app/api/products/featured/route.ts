4a500afe2bbfaf2ce8bdd959dfcf576e38cd7567
Ana_D <ana.marcacini@maua.br>
2024-05-03 16:30:47 -0300

    aula fech

--- Next.js/devstore/src/app/api/products/featured/route.ts
+++ Next.js/devstore/src/app/api/products/featured/route.ts
@@ -5,3 +5,10 @@ export async function GET() {
 // produtos com o featured = true (produtos em destaque)
   return Response.json(featuredProducts)
 }
+
+
+//Isso daqui é uma criação de um backend improvisado -> recomendado o backend ser separado do front
+    // Axios -> é uma forma de fazer solicitações para o backends||  reper em cima de um api antiga da web chamada XMLHttpRequest
+
+    // fech API :  é um axios do navegador 
+      // caching 
---
+++ Next.js/devstore/src/env.ts
@@ -0,0 +1,24 @@
+import { z } from 'zod'
+
+const envSchema = z.object({
+  NEXT_PUBLIC_API_BASE_URL: z.string().url(),
+//   qual formato que eu espero que o NEXT_PUBLIC_API_BASE_URL seja?
+// espero que seja uma string no formato de url
+})
+
+const parsedEnv = envSchema.safeParse(process.env)
+
+if (!parsedEnv.success) {
+//   variavel de ambiente não é uma url
+
+  console.error(
+    'Invalid environment variables',
+    parsedEnv.error.flatten().fieldErrors,
+  ) // flatten - tranforma os erros que deram na validação em um formato mais legivel
+
+  throw new Error('Invalid environment variables.')
+  // para a aplicação
+}
+
+export const env = parsedEnv.data
+// exporta as variaveis de ambiente
