7a79e0829784c2b19288f32baf955de91a2d4b07
Ana_D <ana.marcacini@maua.br>
2024-05-17 15:56:14 -0300

    resolvido : Variáveis ambiente: Client e Server

--- src/env.ts
+++ src/env.ts
@@ -1,45 +1,44 @@
-import { z } from 'zod'
-
-const envSchema = z.object({
-  NEXT_PUBLIC_API_BASE_URL: z.string().url(),
-  APP_URL:z.string().url()
-})
-
-const parsedEnv = envSchema.safeParse(process.env)
-
-if (!parsedEnv.success) {
-//   variavel de ambiente não é uma url
-
-  console.error(
-    'Invalid environment variables',
-    parsedEnv.error.flatten().fieldErrors,
-  ) // flatten - tranforma os erros que deram na validação em um formato mais legivel
-
-  throw new Error('Invalid environment variables.')
-  // para a aplicação
-}
-
-export const env = parsedEnv.data
-// exporta as variaveis de ambiente
-// -------------
-// import { createEnv } from '@t3-oss/env-nextjs'
-// import { createEnv } from "@t3-oss/env-nextjs/dist";
-// import { createEnv } from "@t3-oss/env-core/dist";
 // import { z } from 'zod'
 
-// export const env = createEnv({
-//   server: {
-//     APP_URL: z.string().url(),
-//     //   qual formato que eu espero que o NEXT_PUBLIC_API_BASE_URL seja?
-//     // espero que seja uma string no formato de url
-//   },
-
-//   client: {
-//     NEXT_PUBLIC_API_BASE_URL: z.string().url(),
-//   },
-
-//   runtimeEnv: {
-//     APP_URL: process.env.APP_URL,
-//     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
-//   },
+// const envSchema = z.object({
+//   NEXT_PUBLIC_API_BASE_URL: z.string().url(),
+//   APP_URL:z.string().url()
+// //   qual formato que eu espero que o NEXT_PUBLIC_API_BASE_URL seja?
+// // espero que seja uma string no formato de url
 // })
+
+// const parsedEnv = envSchema.safeParse(process.env)
+
+// if (!parsedEnv.success) {
+// //   variavel de ambiente não é uma url
+
+//   console.error(
+//     'Invalid environment variables',
+//     parsedEnv.error.flatten().fieldErrors,
+//   ) // flatten - tranforma os erros que deram na validação em um formato mais legivel
+
+//   throw new Error('Invalid environment variables.')
+//   // para a aplicação
+// }
+
+// export const env = parsedEnv.data
+// // exporta as variaveis de ambiente
+
+
+import { createEnv } from '@t3-oss/env-nextjs'
+import { z } from 'zod'
+
+export const env = createEnv({
+  server: {
+    APP_URL: z.string().url(),
+  },
+
+  client: {
+    NEXT_PUBLIC_API_BASE_URL: z.string().url(),
+  },
+
+  runtimeEnv: {
+    APP_URL: process.env.APP_URL,
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  },
+})
