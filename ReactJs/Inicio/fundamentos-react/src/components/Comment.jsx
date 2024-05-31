40c7513c01fd7a7177688d650c453ce48618305f
Ana_D <ana.marcacini@maua.br>
2024-05-31 09:30:23 -0300

    curtida

--- ReactJs/Inicio/fundamentos-react/src/components/Comment.jsx
+++ ReactJs/Inicio/fundamentos-react/src/components/Comment.jsx
@@ -1,12 +1,18 @@
 import styles from './Comment.module.css';
+import { useState } from "react";
 
 import { ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from './Avatar';
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0); // iniciando o estado de likes como zero
+
   function handleDeleteComment() {
     onDeleteComment(content);
     console.log("deletar")
+  }
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1);
   }
   return (
     <div className={styles.comment}>
@@ -33,9 +39,9 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
